9f5e96fb8a9aa5f4fd223f75f9bde18246ea6d93
Dan Ventura <danwventura@icloud.com>
2016-10-23 20:57:56 -0500

    controller functions/factory ajax calls built but not working yet

---
+++ FetchNStore/FetchNStore/Scripts/AjaxFactory.js
@@ -0,0 +1,33 @@
+﻿app.factory("AjaxFactory", function ($q, $http) {
+
+    
+    var MakeAjaxGetMethodCall = function (users_input_url) {
+        return $q(function (resolve, reject) {
+            $http.get(users_input_url)
+                .success(function (response) {
+                    console.log("response", response);
+                    resolve(response);
+                })
+        .error(function (error) {
+            reject(error)
+        });
+        })
+    }
+
+    var MakeAjaxHeadMethodCall = function (users_input_url) {
+        return $q(function (resolve, reject) {
+            $http.head(users_input_url)
+                .success(function (response) {
+                    console.log("response", response);
+                    resolve(response);
+                })
+                .error(function (error) {
+                    reject(error)
+        });
+        })
+    }
+
+
+
+    return {MakeAjaxGetMethodCall:MakeAjaxGetMethodCall, MakeAjaxHeadMethodCall:MakeAjaxHeadMethodCall}
+});
---
+++ FetchNStore/FetchNStore/Scripts/Home.Ctrl.js
@@ -0,0 +1,32 @@
+﻿app.controller("HomeCtrl", function($scope, AjaxFactory) {
+
+	$scope.value = 6;
+	$scope.users_input_url;
+	$scope.users_input_method;
+
+	
+
+	$scope.GetAjaxResponseData = function (users_input_url, users_input_method) {
+		console.log("url", users_input_url);
+		console.log("method", users_input_method);
+		
+		if (users_input_method == "GET") {
+
+			$scope.statusCode;
+			$scope.URL;
+			$scope.Method = users_input_method;
+			$scope.responseTime;
+
+			AjaxFactory.MakeAjaxGetMethodCall(users_input_url)
+		}
+		else
+		{
+			$scope.statusCode;
+			$scope.URL;
+			$scope.Method = users_input_method;
+			$scope.responseTime;
+
+			AjaxFactory.MakeAjaxHeadMethodCall(users_input_url)
+		}
+	}	
+})
