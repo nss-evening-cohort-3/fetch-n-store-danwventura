ef0645c8052ec2a932476ed752ec011756395e39
Dan Ventura <danwventura@icloud.com>
2016-10-27 19:28:48 -0500

    added response controller and response class

---
+++ FetchNStore/FetchNStore/Models/Response.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FetchNStore.Models
+{
+    public class Response
+    {
+        [Key]
+        public int Response_ID { get; set; }
+        [Required]
+        public int Status_Code { get; set; }
+        [Required]
+        public string Method { get; set; }
+        [Required]
+        public string URL { get; set; }
+        [Required]
+        public int Response_Time { get; set; }
+    }
+}
