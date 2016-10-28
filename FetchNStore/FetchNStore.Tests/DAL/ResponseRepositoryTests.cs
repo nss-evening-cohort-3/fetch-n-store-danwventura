5ec4f6a23368329a0fa32579bae1eab58e0a41a8
Dan Ventura <danwventura@icloud.com>
2016-10-27 20:03:09 -0500

    first unit test for responserepository and created responsecontext

---
+++ FetchNStore/FetchNStore.Tests/DAL/ResponseRepositoryTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FetchNStore.Models;
+
+namespace FetchNStore.Tests.DAL
+{
+    [TestClass]
+    public class ResponseRepositoryTests
+    {
+        [TestMethod]
+        public void EnsureCanCreateInstanceOfResponseClass()
+        {
+            //Arrange//
+            Response new_response = new Response();
+            //Act//
+
+            //Assert//
+            Assert.IsNotNull(new_response);
+
+        }
+    }
+}
---
+++ FetchNStore/FetchNStore/DAL/ResponseContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FetchNStore.DAL
+{
+    public class ResponseContext : DbContext
+    {
+
+        public virtual DbSet<Responses> {get; set;}
+
+    }
+}
