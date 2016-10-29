5d96b5f63365d89ed644d08a22a41837ed4b0eb4
Dan Ventura <danwventura@icloud.com>
2016-10-29 10:46:29 -0500

    setting up moq in unit tests and edited first unit test

--- FetchNStore/FetchNStore.Tests/DAL/ResponseRepositoryTests.cs
+++ FetchNStore/FetchNStore.Tests/DAL/ResponseRepositoryTests.cs
@@ -1,26 +1,76 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using FetchNStore.Models;
+using Moq;
+using FetchNStore.DAL;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
 
 namespace FetchNStore.Tests.DAL
 {
+    
+
     [TestClass]
     public class ResponseRepositoryTests
     {
+
+        Mock<ResponseContext> mock_context { get; set; }
+        Mock<DbSet<Response>> mock_reponse_table { get; set; }
+        List<Response> response_list { get; set; }
+
+        ResponseRepository repo { get; set; }
+
+        public void ConnectMocksToDataStore()
+        {
+            var queryable_response_list = response_list.AsQueryable();
+
+
+            //      Setting up mock to emulate four behaviors of an iQueryable
+            mock_reponse_table.As<IQueryable<Response>>().Setup(Mock => Mock.Provider).Returns(queryable_response_list.Provider);
+            mock_reponse_table.As<IQueryable<Response>>().Setup(Mock => Mock.Expression).Returns(queryable_response_list.Expression);
+            mock_reponse_table.As<IQueryable<Response>>().Setup(Mock => Mock.ElementType).Returns(queryable_response_list.ElementType);
+            mock_reponse_table.As<IQueryable<Response>>().Setup(Mock => Mock.GetEnumerator()).Returns(queryable_response_list.GetEnumerator);
+
+            //      Have Responses property return our queryable list AKA fake db table
+            mock_context.Setup(a => a.Responses).Returns(mock_response_table.Object);
+
+            //      Callbacks
+
+            mock_response_table.Setup(c => c.Add(It.IsAny<Response>())).Callback((Response a) => response_list.Add(a));
+
+        }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            mock_context = new Mock<ResponseContext>();
+            mock_reponse_table = new Mock<DbSet<Response>>();
+            response_list = new List<Response>();
+            repo = new ResponseRepository(mock_context.Object);
+            ConnectMocksToDataStore();
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            repo = null;
+        }
+
         [TestMethod]
-        public void EnsureCanCreateInstanceOfResponseClass()
+        public void EnsureCanCreateInstanceOfRepo()
         {
             //Arrange//
-            Response new_response = new Response();
+            ResponseRepository new_repo = new ResponseRepository();
             //Act//
 
             //Assert//
-            Assert.IsNotNull(new_response);
+            Assert.IsNotNull(new_repo);
 
         }
 
         [TestMethod]
-        public void ()
+        public void EnsureRepoHasContext()
         {
 
         }
--- FetchNStore/FetchNStore/DAL/ResponseRepository.cs
+++ FetchNStore/FetchNStore/DAL/ResponseRepository.cs
@@ -7,7 +7,17 @@ namespace FetchNStore.DAL
 {
     public class ResponseRepository
     {
+        public ResponseContext Context {get;set;}
+
+        public ResponseRepository()
+        {
+            Context = new ResponseContext();
+        }
 
 
+        public ResponseRepository(ResponseContext _context)
+        {
+            Context = _context;
+        }
     }
 }
